ba8157fc49e5e6bee019f0f444a88e3fbb1b71f4
Tobiah <tobiah@protonmail.com>
2018-08-05 19:11:34 -0500

    feat: react with X if report fails

--- handlers/report.js
+++ handlers/report.js
@@ -7,34 +7,38 @@ const handleReport = async (message, config) => {
   });
   const context = messagesContext.map(msg => `**${msg.author}**\n\t${msg.content} ` +
     `${msg.attachments.length ? 'there are attachments' : ''}`).join('\n');
-
-  await config.reportChannel.send(config.pingOp ? `<@&${config.opRole}>` : undefined, {
-    embed: {
-      color: 0xff0000,
-      title: `New Report from ${message.author.tag}`,
-      fields: [{
-        name: '_ _',
-        value: message.content.replace(`${config.prefix}report`, ''),
-        inline: true,
-      }, {
-        name: 'Channel',
-        value: message.channel.toString(),
-        inline: true,
-      }, {
-        name: 'Reporter',
-        value: message.author.toString(),
-        inline: true,
-      }, {
-        name: 'Voice Channel',
-        value: message.member.voiceChannel ? message.member.voiceChannel.name : 'Memeber not in voice channel',
-        inline: true,
-      }, {
-        name: 'Context',
-        value: context,
-      }],
-      timestamp: new Date(),
-    },
-  });
+  try {
+    await config.reportChannel.send(config.pingOp ? `<@&${config.opRole}>` : undefined, {
+      embed: {
+        color: 0xff0000,
+        title: `New Report from ${message.author.tag}`,
+        fields: [{
+          name: '_ _',
+          value: message.content.replace(`${config.prefix}report`, ''),
+          inline: true,
+        }, {
+          name: 'Channel',
+          value: message.channel.toString(),
+          inline: true,
+        }, {
+          name: 'Reporter',
+          value: message.author.toString(),
+          inline: true,
+        }, {
+          name: 'Voice Channel',
+          value: message.member.voiceChannel ? message.member.voiceChannel.name : 'Memeber not in voice channel',
+          inline: true,
+        }, {
+          name: 'Context',
+          value: context,
+        }],
+        timestamp: new Date(),
+      },
+    }); 
+  } catch (error) {
+    message.react('❌');
+    config.log(error, 'error');
+  }
   await message.delete();
 };
 
